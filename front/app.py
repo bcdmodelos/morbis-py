f1f93d29c26745b6f38027d970933113501fa604
bcdmodelos <brunodedanta@gmail.com>
2025-07-22 00:12:12 -0300

    title

--- front/app.py
+++ front/app.py
@@ -4,7 +4,7 @@ from dash import html
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("MORBIS Dashboard"),
+    html.H1("MorbIS - Mortality Information System"),
     html.P("Aplicação inicial com Dash está rodando!")
 ])
 
