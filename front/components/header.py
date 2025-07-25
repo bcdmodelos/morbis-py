a13f6c1c1c5d5628704683dc132c389179c89616
bcdmodelos <brunodedanta@gmail.com>
2025-07-25 12:42:55 -0300

    header

--- front/components/header.py
+++ front/components/header.py
@@ -1,15 +1,21 @@
 from dash import dcc, html
 
 def render_header():
+    link_style = {
+        "margin-right": "15px",
+        "color": "white",
+        "text-decoration": "none"
+    }
+
     return html.Div(
         [
             html.H2("MorbIS - Mortality Information System", style={"color": "white", "margin": "0"}),
             html.Div([
-                html.A("Home", href="#", style={"margin-right": "15px", "color": "white"}),
-                html.A("Mapa", href="/mapa", style={"margin-right": "15px", "color": "white"}),
-                dcc.Link("CID-10", href="/cid10", style={"margin-right": "15px", "color": "white"}),
-                html.A("Dados", href="/dados", style={"margin-right": "15px", "color": "white"}),
-                html.A("Sobre", href="/sobre", style={"color": "white"})
+                html.A("Home", href="#", style=link_style),
+                html.A("Mapa", href="/mapa", style=link_style),
+                dcc.Link("CID-10", href="/cid10", style=link_style),
+                html.A("Dados", href="/dados", style=link_style),
+                html.A("Sobre", href="/sobre", style={"color": "white", "text-decoration": "none"})
             ], style={"display": "flex"})
         ],
         style={
